09d6f4622089856b6aa7a1c80286daae3e9f6b16
Sonu Singha <deyk64032@gmail.com>
2025-04-27 22:38:28 +0530

    Frontend Navbar created completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+
+
+// Importing Indivisual Components
+
+// Navbar
+import Navbar from "../src/components/navbar/Nav.component.jsx";
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    hello world
+      <Navbar />
     </>
   )
 }
---
+++ Frontend/src/components/navbar/Nav.component.jsx
@@ -0,0 +1,95 @@
+// Code starts from Here
+
+// Importing CSS
+
+import "../navbar/Nav.component.css"
+
+
+
+
+
+// Creating Navbar Component
+
+// Main component
+function Navbar() {
+    return (
+        <div className="Main_Navbar_Component">
+            <ThemeToggle />
+            <ExtraNavButtons />
+        </div>
+    )
+}
+
+
+// Theme Toggle component
+function ThemeToggle() {
+    return (
+        <div className="Main_Theme_Toggle">
+            <div className="theme_logo" alt="theme" />
+            <span className="theme_text">Theme</span>
+
+        </div>
+    )
+}
+
+
+// Extra Nav components
+function ExtraNavButtons() {
+    return (
+        <div className="Main_Extra_Nav_Buttons">
+            <ViewButtonComponent />
+            <CreateButtonComponent />
+            <LogoutComponent />
+        </div>
+    )
+}
+
+
+// View Component
+function ViewButtonComponent() {
+    return (
+        <div className="View_Button_Component">
+            <span className="View_Text">
+            My Posts
+            </span>
+        </div>
+    )
+}
+
+
+// Create Button
+function CreateButtonComponent() {
+    return (
+        <div className="Create_Button_Component">
+            {/* <button className="create_btn" type="button">create</button> */}
+            <img className="create_icon" src="/src/assets/icons/create.icon.svg" alt="create icon" />
+        </div>
+    )
+}
+
+
+// Logout Component
+function LogoutComponent() {
+    return (
+        // <div className="Logout_Component">logout</div>
+        <div className="Logout_Component">
+            <img className="logout_button" src="/src/assets/icons/logout.icon.svg" alt="logout" />
+        </div>
+        
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// exporting the main component
+
+export default Navbar;
