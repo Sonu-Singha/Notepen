d3b10881615fd82733f49a8b2d3b7041c16b9b06
Sonu Singha <deyk64032@gmail.com>
2025-05-06 11:38:55 +0530

    view page fully functional and made connetion with backend and server with the frontend

--- Frontend/src/components/pages/viewpage/HeroComponent.jsx
+++ Frontend/src/components/pages/viewpage/HeroComponent.jsx
@@ -1,11 +1,18 @@
 // Code starts from Here
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./HeroComponent.css"
 import parse from "html-react-parser";
 import 'react-quill/dist/quill.snow.css';
 import "quill/dist/quill.snow.css"
+import axios, { formToJSON } from "axios";
+import { format } from "date-fns";
 
+
+
+// Backend URL
+const BackendURL = import.meta.env.VITE_BACKEND_URL
+const BackendImgPath = "http://localhost:3000/public/uploads/"
 
 
 
@@ -13,12 +20,38 @@ import "quill/dist/quill.snow.css"
 // Main Component(HERO)-----------------------------------------------1
 
 function HeroComponent() {
+
+    const [postID, setPostID] = useState("68199a4c2b7c3bb1ac31014d");//using hard-coded for now!!
+    const [currentPost, setCurrentPost] = useState(null);
+    // console.log(currentPost?.content);
+
+    useEffect(() => {
+        async function fetchPost() {
+            try {
+                const res = await axios.get(`${BackendURL}api/post/${postID}`);
+                // console.log(res.data);
+                setCurrentPost(res.data);
+            } catch (error) {
+                console.log("got error in Frontend while fetching post" + error);
+            }
+        }
+        fetchPost();
+    }, [postID]);
+
+
+
+
     return (
-        <div className="Main_Component_Wrapper">
-            <BannerComponent />
-            <TitleComponent />
-            <ContentComponent />
-        </div>
+        currentPost ?
+            (<div className="Main_Component_Wrapper">
+
+                <BannerComponent post={currentPost} />
+                <TitleComponent post={currentPost} />
+                <ContentComponent post={currentPost} />
+
+            </div>) :
+
+            ("lodaing....")
     )
 }
 
@@ -27,12 +60,15 @@ function HeroComponent() {
 
 // Main Component(Banner)-----------------------------------------------2
 
-function BannerComponent() {
+function BannerComponent({ post }) {
+    if (!post.banner) {
+        return
+    }
     return (
         <div className="Banner_Component">
             <div className="Banner_Container">
                 <img
-                    src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg"
+                    src={BackendImgPath + post.banner}
                     alt="Banner"
                     className="Banner_Image"
                 />
@@ -46,14 +82,16 @@ function BannerComponent() {
 
 // Main Component(Title)-----------------------------------------------2
 
-function TitleComponent() {
+function TitleComponent({ post }) {
+    const date = new Date(post.createdAt);
+    const createdAt = format(date, "d MMMM yyyy")
     return (
         <div className="Title2_Component">
             <div className="Title2_Container">
-                <span className="Title2_Text">First Hello world</span>
+                <span className="Title2_Text">{post.post_title}</span>
                 <div className="creation_credit">
-                    <span className="Date_Text">Created on: 12 March, 2024</span>
-                    <span className="Author_Text">Created By: Demo</span>
+                    <span className="Date_Text">Created on: {createdAt}</span>
+                    <span className="Author_Text">Created By: {post.author_username}</span>
                 </div>
             </div>
         </div>
@@ -65,9 +103,8 @@ function TitleComponent() {
 
 // Main Component(Content)-----------------------------------------------2
 
-function ContentComponent() {
-    const [text, setText] = useState(`<pre class="ql-syntax ql-align-center" spellcheck="false">Hello world
-</pre><p class="ql-align-center"><br></p><p>this something else</p><p class="ql-align-right">scscscscscscscscsc<sub>sccscsc</sub><sup>scscscscscsc</sup>s cscscscsc</p>`)
+function ContentComponent({ post }) {
+    const [text, setText] = useState(post.content)
 
     return (
         <div className="Content2_Component">
